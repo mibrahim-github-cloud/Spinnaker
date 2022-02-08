fbb4c371c04776e0cc753bb81cf9284c0bf66390
mibrahim-github-cloud <52443218+mibrahim-github-cloud@users.noreply.github.com>
2022-02-08 23:32:43 +0530

    Update 3-install-halyard.sh

--- scripts/3-install-halyard.sh
+++ scripts/3-install-halyard.sh
@@ -7,8 +7,8 @@ sudo add-apt-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
 sudo apt-get -y install jq openjdk-11-jdk
 
-
-curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+#curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/debian/InstallHalyard.sh
+curl -O https://raw.githubusercontent.com/spinnaker/halyard/master/install/Installer.sh
 sudo bash InstallHalyard.sh --user ubuntu
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
